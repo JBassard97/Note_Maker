c37dce6e2f591245a486a7a4fc564b6f984e08f0
Jbassard <jonathanacciarito@gmail.com>
2023-11-29 11:22:20 -0600

    struggled to make everything connect with modularization, repopulated server for just for now to make it work

--- helpers/startMiddleware.js
+++ helpers/startMiddleware.js
@@ -1,12 +1,12 @@
-const { app, express } = require("./imports");
+// const { app, express } = require("./imports");
 
-const startMiddleware = () => {
-  // Body-Parsing Middleware
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+// const startMiddleware = () => {
+//   // Body-Parsing Middleware
+//   app.use(express.json());
+//   app.use(express.urlencoded({ extended: true }));
 
-  // "Pointer" Middleware
-  app.use(express.static("public"));
-};
+//   // "Pointer" Middleware
+//   app.use(express.static("public"));
+// };
 
-module.exports = { startMiddleware };
+// module.exports = { startMiddleware };
--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 // Requiring modules
-const { express, path, app } = require("./helpers/imports");
-const { indexHTMLpath } = require("./helpers/serverToHTML");
-const { ListenAt } = require("./helpers/customListen");
-const { startMiddleware } = require("./helpers/startMiddleware");
+const { path, colors } = require("./helpers/imports");
+const { indexHTMLpath, notesHTMLpath } = require("./helpers/serverToHTML");
+// const { ListenAt } = require("./helpers/customListen");
+// const { startMiddleware } = require("./helpers/startMiddleware");
+const express = require("express");
+const app = express();
 
-startMiddleware();
+// Body-Parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, indexHTMLpath)));
+// "Pointer" Middleware
+app.use(express.static("public"));
+
+// Since this is "home", simply routing it here
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, indexHTMLpath)));
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, notesHTMLpath))
+);
 
 // Listening
 const PORT = process.env.PORT || 3001;
+const ListenAt = (PORT) => {
+  app.listen(PORT, () =>
+    console.log(
+      colors.RainbowText(`Note_Taker`),
+      "is listening at",
+      colors.BrightCyanText(`http://localhost:${PORT}`)
+    )
+  );
+};
+
 ListenAt(PORT);
